a0e5db37e753beba79fd0793d9746add2b573443
Lazy Cat <153988610+beingyxq@users.noreply.github.com>
2025-09-27 23:46:16 +0800

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.star.drown"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.star.drown"
         minSdk = 21
         targetSdk = 35
         versionCode = 673
